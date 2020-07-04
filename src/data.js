e17ee58547d807a3af030b2c737d1828ba4e0cfb
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-07-04 14:09:34 -0400

    Create data.js
    add sample data

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export default [
+    { "id": "0837a8f79f4657150c482f9400daea0b", 
+      "title": "Flag",
+      "desc": "The flag of Brazil, is a blue disc depicting a starry sky spanned by a curved band inscribed with the national motto 'Ordem e Progresso'",
+      "url": "https://source.unsplash.com/tncsQE63ENU",
+      "faces": [
+        "http://i.pravatar.cc/300?img=2",
+      ]
+    },
+    { "id": "73000dccc18fd56c9941ee432a96708d", 
+      "title": "Carnaval",
+      "desc": "Carnaval is an annual Brazilian festival held between the Friday afternoon before Ash Wednesday and Ash Wednesday at noon, which marks the beginning of Lent, the forty-day period before Easter.",
+      "url": "https://source.unsplash.com/cCIVkw4K27A",
+      "faces": [
+        "http://i.pravatar.cc/300?img=3",
+      ]
+      
+    },
+    { "id": "33c6b55a4abad5eeaa6b29a3aeee8213", 
+      "title": "Praia dos Carneiros - PE",
+      "desc": "Praia dos Carneiros is a beach located in the city of Tamandaré, in the state of Pernambuco (PE). It is characterized by tall coconut trees, clear and fine sands and calm and warm waters practically all year round.",
+      "url": "https://source.unsplash.com/odII8BzuWU8",
+      "faces": [
+        "http://i.pravatar.cc/300?img=1",
+      ]
+    },
+    { "id": "7e5baef968a2c8ad3254169126247546", 
+      "title": "Recife - PE",
+      "desc": "Recife is the fourth-largest urban area in Brazil with 4,054,866 inhabitants. Founded in 1537, during the early Portuguese colonization of Brazil, it was the main harbor of the Captaincy of Pernambuco and known for its large scale production of sugar cane.",
+      "url": "https://source.unsplash.com/l7fds9EoG1g",
+      "faces": [
+        "http://i.pravatar.cc/300?img=3",
+        "http://i.pravatar.cc/300?img=4"
+      ]
+    },
+    { "id": "528fcca2b4c1bf6d6190f848837c8d09", 
+      "title": "Brazilian",
+      "desc": "Brazilians are citizens of Brazil. Brazil is a multiethnic society, which means that it is home to people of many different ethnic origins. As a result, majority of Brazilians do not equate their nationality with their ethnicity, usually embracing and espousing both simultaneously.",
+      "url": "https://source.unsplash.com/aANIrjIFRKE",
+      "faces": [
+        "http://i.pravatar.cc/300?img=1",
+        "http://i.pravatar.cc/300?img=2",
+      ]
+    },
+    { "id": "b2452085516c81bab68c9f244700d8c1", 
+      "title": "Slum (Favela)",
+      "desc": "A slum is usually a highly populated urban residential area consisting mostly of closely packed, decrepit housing units in a situation of deteriorated or incomplete infrastructure, inhabited primarily by impoverished people.",
+      "url": "https://source.unsplash.com/wGjEiVTy6Z8",
+      "faces": [
+        "http://i.pravatar.cc/300?img=1",
+        "http://i.pravatar.cc/300?img=2",
+        "http://i.pravatar.cc/300?img=4"
+      ]
+    },
+    { "id": "9b1cccf448a2c2df354132875112322", 
+      "title": "Food",
+      "desc": "There is not an exact single 'national Brazilian cuisine', but there is an assortment of various regional traditions and typical dishes. This diversity is linked to the origins of the people inhabiting each area. Brazil really does have it all: moqueca, brigadeiro, acarajé, canjica, feijoada (...)",
+      "url": "https://source.unsplash.com/UuGc5q4Ruxs",
+      "faces": [
+        "http://i.pravatar.cc/300?img=4"
+      ]
+    },
+    { "id": "123fdwws2b1w2ff2e3231f554174c2d92", 
+      "title": "Rio de Janeiro -RJ",
+      "desc": "Rio de Janeiro is known for its natural settings, Carnival, samba, bossa nova, and balneario beaches. In addition to the beaches, some of the most famous landmarks include the giant statue of Christ the Redeemer atop Corcovado mountain, Sugarloaf Mountain with its cable car; the Sambódromo (Sambadrome), and Maracanã Stadium",
+      "url": "https://source.unsplash.com/7F65HDP0-E0",
+      "faces": [
+        "http://i.pravatar.cc/300?img=1",
+
+      ]
+    },
+];
